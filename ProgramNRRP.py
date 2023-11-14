45f48285dcef75e0d2a12226cb3569a286c9ff98
sicnarf14sf <84180843+sicnarf14sf@users.noreply.github.com>
2023-11-14 16:29:03 +0800

    Improved Functionality

--- ProgramNRRP.py
+++ ProgramNRRP.py
@@ -1,6 +1,20 @@
+#Programming Exercise 02: Lexical Analyzer
+"""Programmers:                         Student No.
+    Francis Albert Celeste              2020-09429
+    Alfred Lu                           2020-09814
+    Harold Clyde Valiente               2020-05249
+
+Program Description:
+   The python program has a partial implementation of a compiler for a customized programming language (PL). 
+   A lexical analyzer that has partial implementation requires the software to read custom programming language code, 
+   do lexical analysis, and output the analysis results.
+"""
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+import re
+import os 
 
 class ProgramNRRP(tk.Tk):
     
@@ -9,11 +23,19 @@ class ProgramNRRP(tk.Tk):
         self.title(title)
         self.geometry(size)
         self.state('zoomed')
+        
+        # From Harold
+        self.prod_content = []
+        self.ptbl_content = []
+        self.ptbl_terminals = []
+        self.ptbl_NT = []
+        
         self.left_side = left_section(self)
         self.right_side = right_section(self)
         self.mainloop()
         
     def load_button_command(self):
+        
         file_path = filedialog.askopenfilename(filetypes=[("Production Files", "*.prod;*.ptbl")])
         if file_path:
             self.left_side.status_label.config(text=file_path)
@@ -23,31 +45,18 @@ class ProgramNRRP(tk.Tk):
                 self.ptblCreateLoadTable(file_path)
             else:
                 print("Invalid file type.")
+                
 
     def prod_load_file(self, file_path):
-        # Clear existing data in the Listbox
-        self.left_side.var_used_listbox.delete(0, tk.END)
-
-        # Create a frame within tab_var_used_frame
-        tree_frame = tk.Frame(self.left_side.frame_2)
-        tree_frame.pack( fill=tk.BOTH)
-
-        # Create Treeview
-        self.tree = ttk.Treeview(tree_frame, show="headings")
-
-        # Add vertical scrollbar
-        vsb = ttk.Scrollbar(tree_frame, orient="vertical", command=self.tree.yview)
-        vsb.pack(side="right", fill="y")
-        self.tree.configure(yscrollcommand=vsb.set)
-
-        # Pack Treeview
-        self.tree.pack(expand=tk.YES, fill=tk.BOTH)
-
+        self.check_loaded()
+        
         if file_path:
-            # Clear existing data in the Treeview
-            for item in self.tree.get_children():
-                self.tree.delete(item)
-
+            
+            self.prod_content = []
+            
+            # remove the table
+            self.left_side.remove_prod_table()    
+            
             # Read the content of the selected file
             with open(file_path, "r") as file:
                 lines = file.readlines()
@@ -55,56 +64,114 @@ class ProgramNRRP(tk.Tk):
             # Assuming each line in the file represents a row in the table
             for line in lines:
                 data = line.strip().split(',')
-                self.tree.insert("", "end", values=data)
+                data[0] = int(data[0])
+                self.prod_content.append(data)
+                self.left_side.var_used_production_Treeview.insert("", "end", values=data)
 
             # Assuming the first row contains column headings
-            headings = lines[0].strip().split(',')
-            self.tree["columns"] = headings
+            headings = ['ID', 'NT', 'P']
+            self.left_side.var_used_production_Treeview["columns"] = headings
+            
             for col in headings:
-                self.tree.heading(col, text=col, anchor="center")
-                self.tree.column(col, width=100, anchor="center")  # Adjust the width as needed
-
+                self.left_side.var_used_production_Treeview.heading(col, text=col, anchor="center")
+                self.left_side.var_used_production_Treeview.column(col, width=100, anchor="center")  # Adjust the width as needed
+            
+        self.left_side.var_used_production_Treeview.pack(fill='y', expand=True)
+        self.check_loaded()
 
     def ptblCreateLoadTable(self, file_path):
-        # Clear existing data in the Listbox
-        self.var_used_listbox.delete(0, tk.END)
-
-        # Create a frame within tab_var_used_frame
-        tree_frame = tk.Frame(self.parseFrame)
-        tree_frame.pack(expand=tk.YES, fill=tk.BOTH)
-
-        # Create Treeview
-        self.tree = ttk.Treeview(tree_frame, show="headings")
-
-        # Add vertical scrollbar
-        vsb = ttk.Scrollbar(tree_frame, orient="vertical", command=self.tree.yview)
-        vsb.pack(side="right", fill="y")
-        self.tree.configure(yscrollcommand=vsb.set)
-
-        # Pack Treeview
-        self.tree.pack(expand=tk.YES, fill=tk.BOTH)
-
         if file_path:
-            # Clear existing data in the Treeview
-            for item in self.tree.get_children():
-                self.tree.delete(item)
+            self.left_side.remove_parse_table()
 
+            self.ptbl_content = []
+            self.ptbl_NT = []
+            self.ptbl_terminals = []
+            
             # Read the content of the selected file
             with open(file_path, "r") as file:
                 lines = file.readlines()
 
+            for line in lines[1:]:
+                str = re.split(",", line)
+                self.ptbl_NT.append(str[0])
+                self.ptbl_content.append(str[1:])
+            
             # Extract column headings
             headings = lines[0].strip().split(',')
-            self.tree["columns"] = headings
+            
+            self.ptbl_terminals.append(headings)
+            
+            self.left_side.var_used_parse_treeview["columns"] = headings
             for col in headings:
-                self.tree.heading(col, text=col, anchor="center")
-                self.tree.column(col, width=100, anchor="center")  # Adjust the width as needed
+                self.left_side.var_used_parse_treeview.heading(col, text=col, anchor="center")
+                self.left_side.var_used_parse_treeview.column(col, width=100, anchor="center")  # Adjust the width as needed
 
             # Populate the Treeview with data from the file
             for line in lines[1:]:
                 data = line.strip().split(',')
-                self.tree.insert("", "end", values=data)
-                
+                self.left_side.var_used_parse_treeview.insert("", "end", values=data)
+            
+        self.left_side.var_used_parse_treeview.pack(fill='y', expand=True)
+        self.check_loaded()
+        
+    def parse(self, event=None):
+        def push(prod, stack):
+            prod.reverse()
+            for symbol in prod:
+                if symbol != 'e':
+                    stack.append(symbol)
+            
+        stack = []
+        stack.append('$')
+        stack.append(self.ptbl_NT[0])
+        
+        print(self.prod_content)
+        print(self.ptbl_terminals)
+        print(self.ptbl_NT)
+        print(self.ptbl_content)
+        
+        input_txt = self.left_side.input_field.get()
+        
+        input_buffer = input_txt.split()
+        input_buffer.append('$')
+        
+        print(stack[-1])
+        print(input_buffer)
+        
+        stack_top = stack[-1]
+        curr_input_index = 0
+        
+        while(stack_top != '$'):
+            print(f"before:{stack}")
+            
+            row = self.ptbl_NT.index(stack_top) if stack_top in self.ptbl_NT else None
+            col = self.ptbl_terminals.index(input_buffer[curr_input_index]) if input_buffer[curr_input_index] in self.ptbl_terminals else None
+            
+            if stack_top == input_buffer[curr_input_index]:
+                stack.pop()
+                curr_input_index+=1
+                print(f"after:{stack}")
+            elif stack_top in self.ptbl_terminals:
+                print(f"after:{stack}")
+                print("Error!")
+                break
+            elif row == None or col == None or self.ptbl_content[row][col] == '':
+                print(f"after:{stack}")
+                print("Error adshjasd!")
+                break
+            elif self.ptbl_content[row][col] != '':
+                stack.pop()
+                prod_index = int(self.ptbl_content[row][col]) - 1
+                prod = self.prod_content[prod_index][2].split()
+                push(prod, stack)
+                print(f"after:{stack}")
+            
+            stack_top = stack[-1]      
+        
+    def check_loaded(self):
+        if(self.prod_content != [] and self.ptbl_content != []):
+            self.left_side.parse_button.configure(state=tk.NORMAL)
+            
 class left_section(tk.Frame):
     
     def __init__(self, parent):
@@ -113,11 +180,11 @@ class left_section(tk.Frame):
         super().__init__()
         
         self.parent = parent
-        self.pack(side = "left", fill = "y", padx= 30, pady= [0, 30])
+        self.pack(side = "left", fill = "y", padx= 30, pady= [10, 10])
         
         # Load Button & load-status label
         self.frame_1 = ttk.Frame(self)
-        self.frame_1.pack(side = "top", anchor='nw' ,pady = 15)
+        self.frame_1.pack(side = "top", anchor='nw' ,pady = [0, 15])
         
         # self.load_button = tk.Button(self.frame_1, text="Load", command=self.load_button_command, width=10)
         self.load_button = ttk.Button(self.frame_1, text="Load", command=parent.load_button_command, width=10)
@@ -138,18 +205,18 @@ class left_section(tk.Frame):
 
         # Create variable used display frame
         self.prodFrame = tk.Frame(self, bg="white", relief='sunken', bd=2)
-        self.prodFrame.pack(side = 'top', anchor='w', fill='y', pady=[0, 20])
+        self.prodFrame.pack(side = 'top', anchor='w', fill='y', pady=[0, 10])
         
         # Create scrollbar for the tab_var_used_frame
         self.var_used_scrollbar = ttk.Scrollbar(self.prodFrame, orient='vertical')
         self.var_used_scrollbar.pack(side='right', fill='y')
-
-        # Create a listbox to display the content (you can replace this with your own content)
-        self.var_used_listbox = tk.Listbox(self.prodFrame, yscrollcommand=self.var_used_scrollbar.set, width=40)
-        self.var_used_listbox.pack(fill='y', expand=True)
+        
+         # Create a listbox to display the content (you can replace this with your own content)
+        self.var_used_production_Treeview = ttk.Treeview(self.prodFrame, yscrollcommand=self.var_used_scrollbar.set)
+        self.var_used_production_Treeview.pack(fill='y', expand=True)
 
         # Configure the scrollbar to work with the listbox
-        self.var_used_scrollbar.config(command=self.var_used_listbox.yview)
+        self.var_used_scrollbar.config(command=self.var_used_production_Treeview.yview)
         
         #-----------------------------------------------------PARSE TABLE------------------------------#
         self.frame_3 = ttk.Frame(self)
@@ -161,39 +228,54 @@ class left_section(tk.Frame):
         self.ptable_placeholder = ttk.Label(self.frame_3, text="<placeholder for ptble file>", font=("Helvetica", 8))
         self.ptable_placeholder.pack(side = 'top', pady=[0, 10])
 
+        # PARSE TABLE
         # Create variable used display frame
-        self.parseFrame = tk.Frame(self, bg="white", relief='sunken', bd=2)
-        self.parseFrame.pack(side = 'top', fill = "x", pady=[0, 20])
+        self.parse_frame = tk.Frame(self, bg="white", relief='sunken', bd=2)
+        self.parse_frame.pack(side = 'top', fill = "x")
 
         # Create vertical scrollbar for the tab_var_used_frame
-        self.var_used_scrollbar_y = ttk.Scrollbar(self.parseFrame, orient='vertical')
+        self.var_used_scrollbar_y = ttk.Scrollbar(self.parse_frame, orient='vertical')
         self.var_used_scrollbar_y.pack(side='right', fill='y')
 
         # Create horizontal scrollbar for the tab_var_used_frame
-        self.var_used_scrollbar_x = ttk.Scrollbar(self.parseFrame, orient='horizontal')
+        self.var_used_scrollbar_x = ttk.Scrollbar(self.parse_frame, orient='horizontal')
         self.var_used_scrollbar_x.pack(side='bottom', fill='x')
 
         # Create a listbox to display the content with both vertical and horizontal scrollbars
-        self.var_used_listbox = tk.Listbox(self.parseFrame, yscrollcommand=self.var_used_scrollbar_y.set, xscrollcommand=self.var_used_scrollbar_x.set, width=100)
-        self.var_used_listbox.pack(fill='both', expand=True)
+        self.var_used_parse_treeview = ttk.Treeview(self.parse_frame, yscrollcommand=self.var_used_scrollbar_y.set, xscrollcommand=self.var_used_scrollbar_x.set, height=6)
+        self.var_used_parse_treeview.pack(fill='x', expand=True,)
 
         # Configure the scrollbars to work with the listbox
-        self.var_used_scrollbar_y.config(command=self.var_used_listbox.yview)
-        self.var_used_scrollbar_x.config(command=self.var_used_listbox.xview)
+        self.var_used_scrollbar_y.config(command=self.var_used_parse_treeview.yview)
+        self.var_used_scrollbar_x.config(command=self.var_used_parse_treeview.xview)
 
         #-----------------------------------------------------INPUT SECTION------------------------------#
 
         self.input_label = ttk.Label(self, text="INPUT", font=("Helvetica", 12, "bold"))
-        self.input_label.pack(side = 'top', anchor='w')
+        self.input_label.pack(side = 'top', anchor='w', pady=[10, 0])
 
         self.frame_4 = ttk.Frame(self)
         self.frame_4.pack(side = 'top', anchor='w')
         
-        self.input_field = tk.Text(master = self.frame_4, bg="white", relief='sunken', bd=2, wrap='word', height=1, width=40)
+        self.input_field = tk.Text(master = self.frame_4, bg="white", relief='sunken', bd=2, wrap='word', height=1, width=40, padx=5, pady=5)
         self.input_field.pack(side = 'left', padx=[0, 20])
 
-        self.parse_button = ttk.Button(master = self.frame_4, text="Parse", command="", width="8")
+        self.parse_button = tk.Button(master = self.frame_4, text="Parse", command= parent.parse, width="8", state=tk.DISABLED)
         self.parse_button.pack(side = 'left')
+    
+    def remove_prod_table(self):
+        for item in self.var_used_production_Treeview.get_children():
+                self.var_used_production_Treeview.delete(item)
+
+        self.var_used_production_Treeview.pack_forget()
+        self.var_used_production_Treeview.column("#0", width = 0, stretch = "no")
+    
+    def remove_parse_table(self):
+        for item in self.var_used_parse_treeview.get_children():
+                self.var_used_parse_treeview.delete(item)
+
+        self.var_used_parse_treeview.pack_forget()
+        self.var_used_parse_treeview.column("#0", width = 0, stretch = "no")
         
 class right_section(tk.Frame):
      def __init__(self, parent):
@@ -201,15 +283,15 @@ class right_section(tk.Frame):
         
         super().__init__()
         self.parent = parent
-        self.pack(side = "left", fill = "both", expand = True, padx= 30, pady= [15, 30])
+        self.pack(side = "left", fill = "both", expand = True, padx= 30, pady= [10, 30])
         
         # Label for Parsing section
         
         self.frame_1 = tk.Frame(self)
         self.frame_1.pack(side = "top", anchor='w')
         
-        self.parsing_label = ttk.Label(self.frame_1, text="PRODUCTIONS:", font=("Helvetica", 12, "bold"))
-        self.parsing_label.pack(side = "top")
+        self.parsing_label = ttk.Label(self.frame_1, text="PARSING:", font=("Helvetica", 12, "bold"))
+        self.parsing_label.pack(side = "top", anchor='w')
         
         self.parsing_placeholder = ttk.Label(self.frame_1, text="<placeholder for prod.file>", font=("Helvetica", 8))
         self.parsing_placeholder.pack(side = "top", pady=[0, 10])
